82cd3a8a3fe4293e202fb01a02a9529eb1fb833c
Max Kofler <kofler.max.dev@gmail.com>
2024-11-29 13:12:03 +0100

    Initial CLI and log setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+mod cli;
+use std::process::exit;
+
+use clap::Parser;
+use cli::Cli;
+use colored::Colorize;
+use knightmare::error::Error;
+
 fn main() {
-    println!("Knightmare!");
+    match run() {
+        Ok(v) => exit(v),
+        Err(e) => {
+            println!("{}", e.to_string().red())
+        }
+    }
+}
+
+fn run() -> Result<i32, Error> {
+    let cli = Cli::parse();
+
+    cli.run()
 }
