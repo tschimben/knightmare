817e3f7938c57f2743414a3e744f5971aeafa2dc
Max Kofler <max@acacialinux.org>
2025-01-11 17:05:23 +0100

    Update printout of possible moves for new board

--- src/model/game.rs
+++ src/model/game.rs
@@ -1,8 +1,5 @@
 //! The game and its state
-use crate::{
-    fen::{FromFENError, FromFENString},
-    piece::BoardSquare,
-};
+use crate::fen::{FromFENError, FromFENString};
 
 use super::{board::Board, color::Color};
 
@@ -24,29 +21,18 @@ impl FromFENString for GameState {
         }
         let board = Board::from_fen(elements[0]).unwrap();
         println!("{:#?}", board);
-        for (x, piece) in board.squares.iter().enumerate() {
-            for (y, piece) in piece.iter().enumerate() {
-                if let Some(piece) = piece {
-                    println!(
-                        "{x}:{y} ({piece:?}): {:?}",
-                        piece
-                            .get_all_moves(
-                                &board,
-                                BoardSquare {
-                                    rank: x as u8,
-                                    file: y as u8
-                                }
-                            )
-                            .iter()
-                            .map(|m| {
-                                let (x, y) = m.coordinates();
-                                format!("{x}{y}")
-                            })
-                            .collect::<Vec<String>>()
-                    )
-                }
-            }
+
+        for (coordinate, piece) in board.get_occupied_fields_rm() {
+            println!(
+                "{coordinate} ({piece:?}): {:?}",
+                piece
+                    .get_all_moves(&board, coordinate)
+                    .iter()
+                    .map(|m| { format!("{m}") })
+                    .collect::<Vec<String>>()
+            )
         }
+
         todo!();
     }
 }
